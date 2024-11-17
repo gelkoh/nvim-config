b3d11dbaadbfb60307ffc70a014814f893f85bdf
Gelkoh <ole_kirchner@gmx.de>
2024-11-17 19:30:46 +0100

    Add telescope keybinds

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,7 +1,27 @@
+local keymap = vim.keymap
+
+--------------------------------------------------------------------------------
+-- General
+--------------------------------------------------------------------------------
+
 -- Set leader key to 'space'
 vim.g.mapleader = " "
 
-local keymap = vim.keymap
+--------------------------------------------------------------------------------
+-- Insert mode
+--------------------------------------------------------------------------------
 
 -- Use 'jk' to exit insert mode
 keymap.set("i", "jk", "<ESC>")
+
+--------------------------------------------------------------------------------
+-- Telescope
+--------------------------------------------------------------------------------
+-- For finding files inside the project
+keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>")
+
+-- For finding string inside the project
+keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>")
+
+-- For finding the string the cursor is on inside the project
+keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>")
