7b0917b3d6ffbb38e5e1e0ee1632f9f4508430be
Gelkoh <ole_kirchner@gmx.de>
2024-11-18 20:11:47 +0100

    Add nvim tree, change color scheme to melange

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,13 +1,25 @@
 return {
     {
-        "fenetikm/falcon",
+        "savq/melange-nvim",
         lazy = false,
         priority = 1000,
         config = function()
-            vim.cmd([[colorscheme falcon]])
-        end,
+            vim.cmd([[colorscheme melange]])
+        end
     },
     {
-        'nvim-telescope/telescope.nvim', tag = '0.1.8'
+        'nvim-telescope/telescope.nvim', tag = '0.1.8',
+        lazy = false
+    },
+    {
+        "nvim-tree/nvim-tree.lua",
+        version = "*",
+        lazy = false,
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
+        },
+        config = function()
+            require("nvim-tree").setup {}
+        end
     }
 }
