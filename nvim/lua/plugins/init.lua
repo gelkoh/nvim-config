d9e583e9baabe9c20f0a6fffb187d334da5130e4
Gelkoh <ole_kirchner@gmx.de>
2024-11-20 22:08:43 +0100

    Add lualine plugin

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -26,5 +26,16 @@ return {
         'windwp/nvim-autopairs',
         event = "InsertEnter",
         config = true
+    },
+    {
+        'nvim-lualine/lualine.nvim',
+        dependencies = { 'nvim-tree/nvim-web-devicons' },
+        config = function()
+            require('lualine').setup {
+                options = {
+                    icons_enabled = false,
+                }
+            }
+        end
     }
 }
